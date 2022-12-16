91f5869667aaa79eb465a6bdef95bf3112acb200
Rabbani <rabbanibcs@gmail.com>
2022-12-16 22:07:01 +0600

    done

--- core/settings.py
+++ core/settings.py
@@ -80,10 +80,39 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Database host address:rabbanibcs1924.mysql.pythonanywhere-services.com
+# Username:rabbanibcs1924
+# @rifr@bb@ni
+# @rifr@bb@nibcs
+# $BlogServer
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'BlogServer',
+       'USER': 'postgres',
+       'PASSWORD': '1924',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
+}
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '$BolgServer',
+        'USER': 'rabbanibcs1924',
+        'PASSWORD': 'admin',
+        'HOST':'rabbanibcs1924.mysql.pythonanywhere-services.com',
+        'PORT':'3306',
     }
 }
 
